8da7c371b7a24933ff88f26985cfd1584d188064
Orlando Mendoza <ommv.17@gmail.com>
2024-07-16 03:47:09 -0400

    Inicio de lógica de Bachacos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { MouseEventHandler, useEffect, useRef, useState } from "react";
+import { CSSProperties, MouseEventHandler, useEffect, useRef, useState } from "react";
 import { FaFaceGrinSquint, FaFaceMeh, FaFaceFrown } from "react-icons/fa6";
 import { IoIosWater } from "react-icons/io";
 import { FaPoop } from "react-icons/fa6";
@@ -26,22 +26,141 @@ import { FaUserCircle } from "react-icons/fa";
 
 const base_url = "https://jtnrmwagncsharindxpw.supabase.co/storage/v1/object/public/my-plant-storage"
 
-const Phases = [
-  `${base_url}/0.png`,
-  `${base_url}/1.png`,
-  `${base_url}/2.png`,
-  `${base_url}/3.png`,
-  `${base_url}/4.png`,
-  `${base_url}/5.png`,
-  `${base_url}/6.png`,
-  `${base_url}/7.png`,
+type Fases = {
+  image: string,
+  ants: CSSProperties[],
+}
+
+// CSSProperties
+
+const fase = 6
+
+const imageSize = 9
+const imageHalfSize = `${imageSize / 2}vh`
+
+const fases: Fases[] = [
+  {
+    image: "0.png",
+    ants: [
+      {
+        left: `calc(50vw - ${imageHalfSize} + 1vh)`,
+        bottom: `calc(5vh - ${imageHalfSize})`,
+      },
+    ],
+  },
+  {
+    image: "1.png",
+    ants: [
+      {
+        left: `calc(50vw - ${imageHalfSize} - 1vh)`,
+        bottom: `calc(5vh - ${imageHalfSize})`,
+      },
+    ],
+  },
+  {
+    image: "2.png",
+    ants: [
+      {
+        left: `calc(50vw - ${imageHalfSize} + 5vh)`,
+        bottom: `calc(7vh - ${imageHalfSize})`,
+      },
+    ],
+  },
+  {
+    image: "3.png",
+    ants: [
+      {
+        left: `calc(50vw - ${imageHalfSize} - 1vh)`,
+        bottom: `calc(26vh - ${imageHalfSize})`,
+      },
+      {
+        left: `calc(50vw - ${imageHalfSize} + 9vh)`,
+        bottom: `calc(7vh - ${imageHalfSize})`,
+      },
+    ],
+  },
+  {
+    image: "4.png",
+    ants: [
+      {
+        left: `calc(50vw - ${imageHalfSize} - 1vh)`,
+        bottom: `calc(26vh - ${imageHalfSize})`,
+      },
+      {
+        left: `calc(50vw - ${imageHalfSize} + 9vh)`,
+        bottom: `calc(7vh - ${imageHalfSize})`,
+      },
+      {
+        left: `calc(50vw - ${imageHalfSize} + 2vh)`,
+        bottom: `calc(35vh - ${imageHalfSize})`,
+        transform: "rotateY(180deg)"
+      },
+    ],
+  },
+  {
+    image: "5.png",
+    ants: [
+      {
+        left: `calc(50vw - ${imageHalfSize} - 1vh)`,
+        bottom: `calc(30vh - ${imageHalfSize})`,
+        transform: "rotate(20deg)"
+      },
+      {
+        left: `calc(50vw - ${imageHalfSize} + 9vh)`,
+        bottom: `calc(7vh - ${imageHalfSize})`,
+      },
+      {
+        left: `calc(50vw - ${imageHalfSize} + 6vh)`,
+        bottom: `calc(35vh - ${imageHalfSize})`,
+        transform: "rotateY(180deg)"
+      },
+    ],
+  },
+  {
+    image: "6.png",
+    ants: [
+      {
+        left: `calc(50vw - ${imageHalfSize} - 4vh)`,
+        bottom: `calc(40vh - ${imageHalfSize})`,
+        transform: "rotate(20deg)"
+      },
+      {
+        left: `calc(50vw - ${imageHalfSize} + 9vh)`,
+        bottom: `calc(7vh - ${imageHalfSize})`,
+      },
+      {
+        left: `calc(50vw - ${imageHalfSize} + 7vh)`,
+        bottom: `calc(30vh - ${imageHalfSize})`,
+        transform: "rotateY(180deg) rotate(20deg)"
+      },
+    ],
+  },
+  {
+    image: "7.png",
+    ants: [
+      {
+        left: `calc(50vw - ${imageHalfSize} + 9vh)`,
+        bottom: `calc(7vh - ${imageHalfSize})`,
+      },
+      {
+        left: `calc(50vw - ${imageHalfSize} + 1vh)`,
+        bottom: `calc(38vh - ${imageHalfSize})`,
+        transform: "rotateY(180deg) rotate(-30deg)"
+      },
+      {
+        left: `calc(50vw - ${imageHalfSize} + 0vh)`,
+        bottom: `calc(58vh - ${imageHalfSize})`,
+        transform: "rotate(-40deg)"
+      },
+    ],
+  },
 ]
 
 const Home = () => {
 
   const router = useRouter()
 
-  const [currentPhase, setCurrentPhase] = useState<number>(7)
+  const $image = useRef<HTMLImageElement>(null)
   const $audio = useRef<HTMLAudioElement>(null)
 
   const [renderPage, credentials] = useAuth()
@@ -66,7 +185,10 @@ const Home = () => {
         const service = new PlantService()
 
         const plant = await service.findOne(userId, "userId")
-        setPlant(plant)
+        setPlant({
+          ...plant,
+          currentPhase: fase,
+        })
       }
     })()
   }, [renderPage])
@@ -74,7 +196,7 @@ const Home = () => {
   const handleStartButton: MouseEventHandler<HTMLButtonElement> = ({ currentTarget }) => {
     console.log('currentTarget', { currentTarget })
     if ($audio.current) {
-      $audio.current.play()
+      // $audio.current.play()
       console.log("audio is playing")
       currentTarget.parentElement?.classList.add("!hidden")
     }
@@ -85,14 +207,15 @@ const Home = () => {
     router.push("/sign-in")
   }
 
+  const { image, ants } = fases[plant.currentPhase]
+
   return (
     (user && plant) ?
       <>
         <main className="Home">
-          <div className="Plant" style={{ backgroundImage: `url(${Phases[plant.currentPhase]})` }}></div>
 
           <div className="user-name">
-          <FaUserCircle size={20} /> <span>{user.name}</span>
+            <FaUserCircle size={20} /> <span>{user.name}</span>
           </div>
 
           <div className="actions">
@@ -106,10 +229,30 @@ const Home = () => {
               <MdOutlineLogout size={20} />
             </button>
           </div>
-          
-          <img className="plant" src={Phases[plant.currentPhase]} alt="" />
-          
-          <img className="absolute" width={30} height={30} src={`${base_url}/bachaco.webp`} alt="" />
+
+          <img className="plant" ref={$image} src={`${base_url}/${image}`} alt="" />
+
+          {
+            $image.current &&
+            ants.map((styles) =>
+              <img
+                className="bachaco"
+                // width={40}
+                // height={40}
+                // style={{
+                //   left: `${($image.current as HTMLImageElement).width * left}%`,
+                //   bottom: `${($image.current as HTMLImageElement).height * bottom}%`,
+                // }}
+                style={styles}
+                onResize={() => {
+                  console.log("Hola")
+                }}
+                // src={`${base_url}/bachaco.webp`}
+                src={`https://images.vexels.com/media/users/3/312193/isolated/preview/27837f45221dd465567ad49ac507bc51-hormiga-trabajadora.png`}
+                onClick={() => alert("Hello")}
+              />
+            )
+          }
 
           <div className="plant-state">
             <FaFaceGrinSquint size={40} className="fill-emerald-500" />
