025b2bdf4febbaec141fd6602a2d767291750553
Orlando Mendoza <ommv.17@gmail.com>
2024-07-17 08:49:29 -0400

    Creación de Variable de entorno, BaseUrl

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL
+
 const API = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL,
   timeout: 3000,
   headers: {'Content-Type': 'application/json'}
 });
